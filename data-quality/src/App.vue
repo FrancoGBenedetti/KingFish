8b0501d2943daa2c9ca7602756578f1ffcf7ea16
Franco Gustavo Benedetti David <birdman@Francos-MacBook-Pro.local>
2020-03-18 16:33:49 -0300

    initial

---
+++ data-quality/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <side-bar />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import SideBar from './components/SideBar'
+export default {
+  data: () => ({
+  }),
+  components: {
+    'side-bar': SideBar
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
---
+++ data-quality/src/components/SideBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="side-bar-container">
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link> 
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () =>({
+    name: ''
+  }),
+  methods: {
+    goTo(route){
+      this.$router.push({ name: route})
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
